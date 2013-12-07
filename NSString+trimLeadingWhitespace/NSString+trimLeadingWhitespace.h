97fbb9f80e8c5772e8379cf50313fe7066d32778
Alan Lail <alan.m.lail@gmail.com>
2013-12-06 21:55:48 -0500

    Added NSString+trimLeadingWhitespace and ignore file

---
+++ NSString+trimLeadingWhitespace/NSString+trimLeadingWhitespace.h
@@ -0,0 +1,11 @@
+
+@interface NSString (trimLeadingWhitespace)
+
+/**
+ Trims leading whitespace from the provided NSString object.
+ 
+ @return A new string with the leading whitespace removed.
+ */
+-(NSString*)stringByTrimmingLeadingWhitespace;
+
+@end
---
+++ NSString+trimLeadingWhitespace/NSString+trimLeadingWhitespace.m
@@ -0,0 +1,16 @@
+
+#import "NSString+trimLeadingWhitespace.h"
+
+@implementation NSString (trimLeadingWhitespace)
+
+-(NSString*)stringByTrimmingLeadingWhitespace {
+    NSInteger i = 0;
+    
+    while ((i < [self length])
+           && [[NSCharacterSet whitespaceCharacterSet] characterIsMember:[self characterAtIndex:i]]) {
+        i++;
+    }
+    return [self substringFromIndex:i];
+}
+
+@end
